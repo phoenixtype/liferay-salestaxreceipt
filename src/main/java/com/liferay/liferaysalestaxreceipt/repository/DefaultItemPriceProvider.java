b6b6554753acd3bce1473fd7f631dbbfbcde3aa3
Samuel Akuma <usakuma@uwaterloo.ca>
2023-04-08 14:18:08 -0400

    Modified Sales Tax calculation code

--- src/main/java/com/liferay/liferaysalestaxreceipt/repository/DefaultItemPriceProvider.java
+++ src/main/java/com/liferay/liferaysalestaxreceipt/repository/DefaultItemPriceProvider.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class DefaultItemPriceProvider implements ItemPriceProvider {
     @Override
     public BigDecimal getItemPrice(String itemName) {
-        // TODO: implement method to look up item price in a database or service
+        //TODO : implement method to look up item price in a database or service
         // For this example, just return a default price of $1.00
         return new BigDecimal("1.00");
     }
